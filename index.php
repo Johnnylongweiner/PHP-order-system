9040911f2f77d288ba2876d78192e4a17d5f94b5
Johnnylongweiner <john.inez787@gmail.com>
2025-04-29 22:31:18 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$products = [
+    "Fried Chicken" => 130,
+    "Roasted Chicken" => 140,
+    "Beef Stake" => 145,
+    "Pork Chop" => 125
+];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $orderSummary = "";
+    $totalAmount = 0;
+    foreach ($products as $product => $price) {
+        $qty = (int)($_POST['qty'][$product] ?? 0);
+        if ($qty > 0) {
+            $cost = $qty * $price;
+            $orderSummary .= "$qty PC of $product. P$cost<br>";
+            $totalAmount += $cost;
+        }
+    }
+    $orderSummary = "You ordered:<br>$orderSummary<strong>Total: P$totalAmount</strong>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ordering System</title>
+</head>
+<body>
+    <h2>Ordering System</h2>
+    <form method="POST">
+        <?php foreach ($products as $product => $price): ?>
+            <div>
+                <label>
+                    <?= $product ?> - P<?= $price ?>
+                </label>
+                <input type="number" name="qty[<?= $product ?>]" value="0">
+            </div>
+        <?php endforeach; ?>
+        <br>
+        <button type="submit">Add to basket</button>
+    </form>
+
+    <?php if (isset($orderSummary)): ?>
+        <div>
+            <?= $orderSummary ?>
+        </div>
+    <?php endif; ?>
+</body>
+</html> 
